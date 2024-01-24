80ec889bb8fc03fe0251ffbd09a99699505fb21c
RafaelLeal1 <126499657+RafaelLeal1@users.noreply.github.com>
2024-01-24 16:36:37 -0300

    Add all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
 
-function App() {
+import api from './services/api.js'
+
+function App() {  
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({}); 
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+
+  async function handleSearch() {
+    // 35900279/json
+
+    if(input === '') {
+      alert('Preenche algum CEP!')
+      return;
+    } 
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("");
+    } catch {
+        alert("Erro ao buscar")
+        setInput("")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+  //<script src="script.js"></script>
+  //<script src="https://kit.fontawesome.com/998c60ef77.js" crossorigin="anonymous"></script>
+
+    <div className="container">
+      <h1 className="title">CEP Tracker</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep..." value={input} onChange={(e) => setInput(e.target.value) } onKeyDown={handleKeyDown}/>
+
+
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='#FFF' />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className='main'> 
+        <h2>CEP: {cep.cep} </h2>
+
+        <span>{cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade} - {cep.uf}</span>
+    </main>
+      )}
+      
+      <input type="checkbox" class="checkbox" id="chk" />
+    <label class="label" for="chk">
+      <i class="fas fa-moon"></i>
+      <i class="fas fa-sun"></i>
+      <div class="ball"></div>
+    </label>
     </div>
+      
+
   );
 }
 
